726e5814bfa6a5ad99e556e79fdc3ad5025bb2ae
PRASHANT RAJ SHUKLA <prashantrajshukla@192.168.1.16>
2024-07-24 18:07:30 +0530

    First commit

---
+++ src/main/java/com/hackathon/skillhunt/SkillhuntApplication.java
@@ -0,0 +1,14 @@
+package com.hackathon.skillhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillhuntApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkillhuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/skillhunt/SkillhuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.skillhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillhuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
